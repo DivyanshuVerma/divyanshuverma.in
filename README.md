48247f3636174f33afc9f9171b646dfc908f5fd2
Divyanshu Verma <dev.verma1010@gmail.com>
2019-06-27 21:32:07 +0530

    Setting up webpack and a hello world webpage

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # divyanshuverma.in
+
+# To build
+npx webpack --mode=production
+
+# To run
+node server.js
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  entry: ['./src/app/index.js', './src/styles/index.css'],
+  optimization: {
+    minimizer: [new TerserPlugin()],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + "/src/public/index.html",
+      inject: 'body',
+      collapseWhitespace: true,
+
+    }),
+    new CopyWebpackPlugin([{
+      from: __dirname + '/src/public'
+    }])
+  ]
+};
