52032365bf42e55681d4a5e773b902cd63ed658c
Divyanshu Verma <dev.verma1010@gmail.com>
2019-07-02 19:49:03 +0530

    Minifying html

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,15 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
         test: /\.s?css$/,
         use: [
           {
